c5e92bfc9881667f38743d2d704b72ad6d0c71d4
floppyhammer <tannhauser_chen@outlook.com>
2023-05-25 13:57:57 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to Use
+
+1. Run `cargo build` in the Rust manifest directory.
+
+2. Copy the generated dynamic library to `bin`.
+
+3. Build CMake to run the test.
--- third_party/font_kit/build.rs
+++ third_party/font_kit/build.rs
@@ -4,6 +4,8 @@ use std::path::PathBuf;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
+
+    // Where to put the generated header file.
     let output_file = PathBuf::from(&crate_dir)
         .join("include")
         .join(format!("{}.h", package_name));
